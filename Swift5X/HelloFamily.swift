1968de94c249ece0321604c37eb4e01f88e6c52a
Christopher Ray <vde435@gmail.com>
2022-10-01 15:08:50 -0400

    Create HelloFamily.swift

---
+++ Swift5X/HelloFamily.swift
@@ -0,0 +1,11 @@
+//not tested yet
+
+var name1 = "Jon"
+var name2= "Kim"
+var name3 = "Kailey"
+var name4 = "Kara"
+
+var line = ""
+
+print(name1, name2, name3, name4, seperator:", ", terminator:"", to:&line)
+print(line)
