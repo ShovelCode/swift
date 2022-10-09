9098cc99ee10c43dc53474b97918a6f4e673b94d
Christopher Ray <vde435@gmail.com>
2022-10-09 13:50:23 -0500

    Create multiline-strings.swift

---
+++ Swift5X/multiline-strings.swift
@@ -0,0 +1,10 @@
+//multiline strings
+let longBook = """
+ In the end of the century
+    some may look back and think
+    well I am glad that canal was dredged.
+ """
+
+for char in longBook {
+    print(char);
+}
